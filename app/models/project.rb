9b57fa0ca98602f0002bc6aeb9af69d14e83f701
Dennis Choi <dennis.choi7@gmail.com>
2016-05-05 18:38:40 -0400

    - In project.rb added a date_left method to calculate said method
    - Then on the project.show we called this method

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,5 +5,10 @@ class Project < ActiveRecord::Base
 
   belongs_to :owner, class_name: "User"
 
-	accepts_nested_attributes_for :rewards
+  accepts_nested_attributes_for :rewards
+
+
+  def date_left
+    project_days_length = (self.end_date - Date.today).to_i
+  end
 end
